2a1825c13d72701eb39971cf6d6fb6760f7fea3f
kristina378 <kristinadubiaha@gmail.com>
2024-11-05 00:00:12 +0100

    "Test version of DyniamicArray.test.cpp"

--- tests/DynamicArray.test.cpp
+++ tests/DynamicArray.test.cpp
@@ -7,7 +7,7 @@ TEST_CASE("Constructor DynamicArray<int,5>", "[DynamicArray]") {
     REQUIRE(array1.get(1) == 2);
     REQUIRE(array1.get(2) == 3);
     REQUIRE(array1.get(3) == 4);
-    REQUIRE(array1.get(3) == 5);
+    REQUIRE(array1.get(4) == 5);
 }
 
 TEST_CASE("DynamicArray<int,5>::begin; DynamicArray<int,5>::end", "[DynamicArray]") {
@@ -32,3 +32,23 @@ TEST_CASE("DynamicArray<int,5>::begin; DynamicArray<int,5>::end", "[DynamicArray
     // End
     REQUIRE(iterator == array1.end());
 }
+
+TEST_CASE("DynamicArray<int,5>::set","[DynamicArray]") {
+    DynamicArray<int> array = {1, 2, 3, 4, 5};
+    array.set(0, 3);
+    array.set(1, 4);
+    array.set(2, 5);
+    array.set(3, 6);
+    array.set(4, 7);
+
+    REQUIRE(array.get(0) == 3);
+    REQUIRE(array.get(1) == 4);
+    REQUIRE(array.get(2) == 5);
+    REQUIRE(array.get(3) == 6);
+    REQUIRE(array.get(4) == 7);
+};
+TEST_CASE("DynamicArray<int,5>::push","[DynamicArray]"){
+DynamicArray<int> array = {1, 2, 3, 4, 5};
+array.push(12);
+REQUIRE(array.get(5)==12);
+};
